2758af1acc86502e666913fd7be1372de84ccc93
vladislav_dvortsov <dvortsov.vlad@gmail.com>
2021-09-23 21:24:35 +0300

    Добавление небольшого функционала

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,30 @@
 import './swiper'
+import './mask'
+
+const selectionTours = document.querySelector('.tours-section');
+const selectionCallback = document.querySelector('.callback-section');
+const selectionReview = document.querySelector('.review-section');
+
+const btnTours = document.querySelector('.menu-item-tours');
+const btnCallback = document.querySelector('.menu-item-callback');
+const btnReview = document.querySelector('.menu-item-review');
+
+const scrollToSection = (elem) => {
+    window.scroll({
+        left: 0,
+        top: elem.offsetTop,
+        behavior: 'smooth',
+    })
+}
+
+btnTours.addEventListener('click', () => {
+    scrollToSection(selectionTours)
+});
+
+btnCallback.addEventListener('click', () => {
+    scrollToSection(selectionCallback)
+});
+
+btnReview.addEventListener('click', () => {
+    scrollToSection(selectionReview)
+});
--- src/js/swiper.js
+++ src/js/swiper.js
@@ -4,7 +4,6 @@ const swiperHotPlaces = new Swiper('.swiper-container', {
         spaceBetween: 30,
         slidesPerView: 3,
         speed: 500,
-        // loop: true,
         autoplay: {
             delay: 200,
         },
